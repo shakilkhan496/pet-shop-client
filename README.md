a881c2e3c0a7ecff7e22651bde51cc0deb5838b8
shakilkhan496 <shakilkhan496@gmail.com>
2023-04-23 19:36:51 +0600

    env updated

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Welcome to Pet shop
-### This is a multi-dimensional business website 
+### Introducing "Pet Shop," a multi-dimensional business website that offers a variety of pet-related services and products.Built with the latest technologies such as ReactJS, Tailwind CSS, Firebase, MongoDB, JWT, dotenv, and Stripe, this website is designed to provide a seamless user experience with secure online transactions.Whether you're looking to adopt a pet, purchase pet supplies, or learn about pet care, "Pet Shop" has something for everyone. 
 #Technologies
 ### React js , Tailwind css , Firebase , MongoDB , JWT , dotenv , Stripe and so on.
 
--- src/Pages/DashBoard/Payment/Payment.js
+++ src/Pages/DashBoard/Payment/Payment.js
@@ -6,8 +6,12 @@ import { useLoaderData, useNavigation } from 'react-router-dom';
 import Loading from '../../../components/Loading/Loading';
 import { AuthContext } from '../../../contexts/AuthProvider';
 import CheckOut from '../../../shared/CheckOut/CheckOut';
+import AccountSubscription from './Subscription';
+import Register from './Subscription';
+import ProductDisplay from './Subscription';
+import Subscription from './Subscription';
 
-const stripePromise = loadStripe('pk_test_51M5vv4CLTcmkmHRYbLOL5pjvoNfQe0mptVirDWj3RH2BEY4UcmefgIgys2i8Ax1l5qXILfQyTWeU2ZfUAVOd4ArQ00ZSQnlgsK');
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 const Payment = () => {
     const paymentData = useLoaderData();
     const navigation = useNavigation();
@@ -24,7 +28,8 @@ const Payment = () => {
 
             <div className='px-5 lg:w-2/3 lg:mx-auto mx-5 rounded-xl  mt-10 shadow-xl py-10'>
                 <Elements stripe={stripePromise}>
-                    <CheckOut paymentData={paymentData}></CheckOut>
+                    {/* <CheckOut paymentData={paymentData}></CheckOut> */}
+                    <Subscription></Subscription>
                 </Elements>
             </div>
 
